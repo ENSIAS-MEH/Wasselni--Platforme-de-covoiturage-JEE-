ff6cc764db5414398067a8ce08245aa6c2006174
IsmailBarkani <ismail.barkani@um5s.net.ma>
2020-01-23 19:17:44 +0100

    Phase 2 complet (not ready to use)

--- web/ressources/js/global.js
+++ web/ressources/js/global.js
@@ -68,4 +68,54 @@
         console.log(e);
     }
 
+ /*==================================================================
+        [ Input prix (debugage :'(  ) ]*/
+
+    try {
+    
+        var inputCon = $('.js-prix-input');
+    
+        inputCon.each(function () {
+            var that = $(this);
+    
+            var btnPlus = that.find('.plus');
+            var btnMinus = that.find('.minus');
+            var qtyInput = that.find('.montant');
+    
+    
+            btnPlus.on('click', function () {
+                var max = 500;
+                
+                var oldValue = parseInt(qtyInput.val());
+                if (oldValue >= max) {
+                    var newVal = oldValue;
+                } else {
+                    var newVal = oldValue + 50;
+                }
+                
+                qtyInput.val(refineString(newVal));
+            });
+    
+            btnMinus.on('click', function () {
+                var min = 50;
+    
+                var oldValue = parseInt(qtyInput.val());
+                if (oldValue <= min) {
+                    var newVal = oldValue;
+                } else {
+                    var newVal = oldValue - 50;
+                }
+                qtyInput.val(refineString(newVal));
+            });
+        });
+    
+        function refineString(s) {
+            if(parseInt(s) == 1) return parseInt(s) + " Dhs";
+            else return parseInt(s) + " Dhs";
+        }
+    
+    }catch (e) {
+        console.log(e);
+    }
+
 })(jQuery);
