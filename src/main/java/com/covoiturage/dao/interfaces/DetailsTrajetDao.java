a9da5df5c9fe6d00cedae53d8c55f685961ef189
boufousabd <abdellah.boufous@um5s.net.ma>
2020-01-24 15:59:12 +0100

    Adding DetailsTrajetDao interface and its implementation

---
+++ src/main/java/com/covoiturage/dao/interfaces/DetailsTrajetDao.java
@@ -0,0 +1,14 @@
+package com.covoiturage.dao.interfaces;
+
+import com.covoiturage.beans.DetailsTrajet;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface DetailsTrajetDao {
+
+    public DetailsTrajet findSpecificDetailsTrajet(DetailsTrajet detailsTrajet) throws SQLException;
+    public List<DetailsTrajet> findAllDetailsTrajets() throws SQLException;
+    public Long insertDetailsTrajet(DetailsTrajet detailsTrajet) throws SQLException;
+    public boolean updateDetailsTrajet(DetailsTrajet detailsTrajet) throws SQLException;
+}
