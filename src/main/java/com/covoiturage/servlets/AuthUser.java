35d1589a6049d66a35cfcb54b6ed84a1ff997867
DarkIlyas <ilyas.bouziane@um5s.net.ma>
2020-02-02 14:51:01 +0100

    update AuthUser servlet

--- src/main/java/com/covoiturage/servlets/AuthUser.java
+++ src/main/java/com/covoiturage/servlets/AuthUser.java
@@ -1,13 +1,19 @@
 package com.covoiturage.servlets;
 
+import com.covoiturage.forms.UserAuthForm;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 public class AuthUser extends HttpServlet {
+    private static final String ATT_SESSION_USERID = "userId";
+
     private static final String VUE_AUTHENTIFICATION = "/WEB-INF/authentification.jsp";
+    private static final String VUE_USER_ACCUEIL = "";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -16,6 +22,16 @@ public class AuthUser extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        UserAuthForm form = new UserAuthForm();
+        int userId = form.authentification(req);
+
+        if(form.getErreurs().isEmpty()){
+            HttpSession session = req.getSession();
+            session.setAttribute(ATT_SESSION_USERID,userId);
+
+            this.getServletContext().getRequestDispatcher(VUE_USER_ACCUEIL).forward(req,resp);
+        } else {
+            this.getServletContext().getRequestDispatcher(VUE_AUTHENTIFICATION).forward(req, resp);
+        }
     }
 }
