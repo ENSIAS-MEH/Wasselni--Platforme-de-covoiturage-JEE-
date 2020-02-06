b5834fa8ee9124f5ccb939f236b70fad99bf57f1
DarkIlyas <ilyas.bouziane@um5s.net.ma>
2020-02-06 14:24:34 +0100

    consulter offres avancement : missing dao methods

--- src/main/java/com/covoiturage/servlets/ConsulterOffres.java
+++ src/main/java/com/covoiturage/servlets/ConsulterOffres.java
@@ -1,5 +1,7 @@
 package com.covoiturage.servlets;
 
+import com.covoiturage.dao.DAOFactory;
+import com.covoiturage.dao.interfaces.UserDao;
 import com.covoiturage.forms.OffreForm;
 
 import javax.servlet.ServletException;
@@ -10,13 +12,19 @@ import java.io.Console;
 import java.io.IOException;
 
 public class ConsulterOffres extends HttpServlet {
+    static final String DAO_FACTORY  = "daofactory";
+    private UserDao userDao;
+
+    @Override
+    public void init() throws ServletException {
+        this.userDao = ((DAOFactory) this.getServletContext().getAttribute(DAO_FACTORY)).getUserDao();
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        OffreForm form = new OffreForm();
+        OffreForm form = new OffreForm(userDao);
         form.consulterOffres(req);
-
-        this.getServletContext().getRequestDispatcher("/test.jsp").forward(req,resp);
+        this.getServletContext().getRequestDispatcher("/offres.jsp").forward(req,resp);
 
     }
 }
