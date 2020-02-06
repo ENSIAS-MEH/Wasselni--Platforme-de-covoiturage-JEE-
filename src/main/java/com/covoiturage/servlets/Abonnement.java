d93e23653bca05f859f676dedc3ccdcf43f6712d
DarkIlyas <ilyas.bouziane@um5s.net.ma>
2020-02-06 14:04:50 +0100

    avancement abonnement

--- src/main/java/com/covoiturage/servlets/Abonnement.java
+++ src/main/java/com/covoiturage/servlets/Abonnement.java
@@ -1,5 +1,7 @@
 package com.covoiturage.servlets;
 
+import com.covoiturage.dao.DAOFactory;
+import com.covoiturage.dao.interfaces.UserDao;
 import com.covoiturage.forms.AbonnementForm;
 
 import javax.servlet.ServletException;
@@ -9,14 +11,23 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class Abonnement extends HttpServlet {
-    private static final String VUE_APRE_ABONNEMENT = "";
+    private static final String VUE_APRE_ABONNEMENT = "/index.jsp";
 
+    static final String DAO_FACTORY  = "daofactory";
+    private UserDao userDao;
+
+    private static final String ATT_FORM = "form";
+
+    @Override
+    public void init() throws ServletException {
+        this.userDao = ((DAOFactory) this.getServletContext().getAttribute(DAO_FACTORY)).getUserDao();
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        AbonnementForm form = new AbonnementForm();
+        AbonnementForm form = new AbonnementForm(userDao);
         form.inscrire(req);
-
+        req.setAttribute(ATT_FORM,form);
         this.getServletContext().getRequestDispatcher(VUE_APRE_ABONNEMENT).forward(req,resp);
     }
 }
