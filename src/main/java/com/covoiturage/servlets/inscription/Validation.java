04107a8fa8f309c53939f4d2db5ccf253d0ca479
DarkIlyas <ilyas.bouziane@um5s.net.ma>
2020-02-06 22:06:48 +0100

    update validation form

--- src/main/java/com/covoiturage/servlets/inscription/Validation.java
+++ src/main/java/com/covoiturage/servlets/inscription/Validation.java
@@ -17,9 +17,11 @@ public class Validation extends HttpServlet {
     private UserDao userDao;
 
     private static final String ATT_SESSION_USERID = "userId";
+    private  static  final String ATT_FORM = "form";
 
     private static final String VUE_USER_ACCUEIL = "/WEB-INF/espace/user/espace_user.jsp";
     private static final String VUE_APRES_INSCRIPTION = "/apres_inscription.jsp";
+
 
     @Override
     public void init() throws ServletException {
@@ -30,6 +32,7 @@ public class Validation extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ValidationForm form = new ValidationForm(userDao);
         User userActivated = form.validate(req);
+        req.setAttribute(ATT_FORM , form);
         HttpSession session = req.getSession();
         if(form.getErreurs().isEmpty()){
             session.setAttribute(ATT_SESSION_USERID,userActivated.getId());
